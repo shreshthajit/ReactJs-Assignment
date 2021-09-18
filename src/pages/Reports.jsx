2f731af9fd24c1cbbea3dd918413c559fc5ca3bf
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2021-09-18 20:33:48 +0600

    Update Reports.jsx

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,11 +1,11 @@
-// import React from 'react';
+import React from 'react';
 
-// function Reports() {
-//   return (
-//     <div className='reports'>
-//       <h1>Reports</h1>
-//     </div>
-//   );
-// }
+function Reports() {
+  return (
+    <div className='reports'>
+      <h1>Reports</h1>
+    </div>
+  );
+}
 
-// export default Reports;
+export default Reports;
