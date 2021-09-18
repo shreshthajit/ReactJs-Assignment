638845ccbe4cdbf7d1d72a09eb4a9f948bed3c2f
Shreshthajit Das <43321488+shreshthajit@users.noreply.github.com>
2021-09-18 20:33:01 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,5 @@
 // import React from 'react';
-// import './App.css';
- 
-// import Navbar from './Component/Navbar';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// import Home  from './pages/Home';
-// import Reports from './pages/Reports';
-// import Products from './pages/Products';
-
-// function App() {
-//   return (
-//     <>
-//       <Router>
-//         <Navbar />
-//         <Switch>
-//           <Route path='/' exact component={Home} />
-//           <Route path='/reports' component={Reports} />
-//           <Route path='/products' component={Products} />
-//         </Switch>
-//       </Router>
-//     </>
-//   );
-// }
-
-// export default App;
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import { Home } from "./pages/Home";
